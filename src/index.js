15f3c7ce2c4127787a48be405c0c125edb152fa9
moranja <jeffadammoran@gmail.com>
2019-05-09 10:35:27 -0500

    done

--- src/index.js
+++ src/index.js
@@ -5,51 +5,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = { count: 0 };
+
+  updateCount = (plusMinus) => {
+    if (plusMinus === "+") {
+      this.setState(prevState => ({ count: prevState.count + 1 }))
+    } else {
+      this.setState(prevState => ({ count: prevState.count - 1 }))
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Counter />
+        <Header count={this.state.count} />
+        <Counter count={this.state.count} updateCount={this.updateCount} />
       </div>
     );
   }
 }
 
 class Header extends Component {
+
+  renderDescription = () => {
+    const remainder = this.props.count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${this.props.count + upToNext}`;
+  };
+
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
+        <h3>{this.renderDescription()}</h3>
       </header>
     );
   }
 }
 
 class Counter extends Component {
-  state = { count: 0 };
-
-  increment = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
-  };
-
-  decrement = () => {
-    this.setState(prevState => ({ count: prevState.count - 1 }));
-  };
-
-  renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
-  };
 
   render() {
     return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        <h1>{this.props.count}</h1>
+        <button onClick={() => this.props.updateCount("-")}> - </button>
+        <button onClick={() => this.props.updateCount("+")}> + </button>
       </div>
     );
   }
